19c2cb176ff87e7179de13ef3240febaa949076e
clj <1258783720@qq.com>
2025-11-10 13:47:08 +0800

    1

--- deploy/MetaNodeStake.js
+++ deploy/MetaNodeStake.js
@@ -1,16 +1,20 @@
 const {upgrades,ethers,network} = require('hardhat');
-require('dotenv').config();
-// sepoliaAddress : 0x9dFce74DEbC4d9a1456965bb4F81f257c577Bb2d
+const fs = require("fs");
+/** 
+ *  MetaNodeStake deployed to: 0x2C8dB0901c9EaD832DE6A83FA749A5a025f0Fa7f
+ℹ️ Implementation: 0x6284E87751CA94c20f6D854e8758C91430A2F8f5
+ */
 module.exports = async ({getNamedAccounts}) => {
     const { deployer } = await getNamedAccounts();
     const contractFactory = await ethers.getContractFactory("MetaNodeStake");
+    config = JSON.parse(fs.readFileSync("./config.json"));
     const blockNum = await ethers.provider.getBlockNumber();
     const contract = await upgrades.deployProxy(
     contractFactory,
     [
       config.token[network.name].tokenAddress,
       blockNum, // _startBlock
-      blockNum, // _endBlock
+      blockNum+10000, // _endBlock
       0, // _metaNodePerBlock
     ],
     {
@@ -22,7 +26,7 @@ module.exports = async ({getNamedAccounts}) => {
     contractAddress = await contract.getAddress();
     console.log("MetaNodeStake deployed to:", contractAddress);
     console.log("ℹ️ Implementation:", await upgrades.erc1967.getImplementationAddress(await contract.getAddress()));
-    
+
 }
 
 module.exports.tags = ["MetaNodeStake"];
--- deploy/MetaNodeToken.js
+++ deploy/MetaNodeToken.js
@@ -2,8 +2,8 @@ const {upgrades,ethers} = require('hardhat');
 
 // sepoliaAddress : 0x9dFce74DEbC4d9a1456965bb4F81f257c577Bb2d
 /**
- * MetaNodeToken deployed to: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
-ℹ️ Implementation: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+ * MetaNodeToken deployed to: 0x2140672baa3E6184247D7ac26d0Bc066172c85Ff
+ℹ️ Implementation: 0x8B65bEd4239Dc59399D8cb959c90F86C8F8a4fd7
  **/
 module.exports = async ({getNamedAccounts}) => {
     const { deployer } = await getNamedAccounts();
@@ -13,11 +13,12 @@ module.exports = async ({getNamedAccounts}) => {
     [
       "MetaNodeToken", // 
       "MNT", // symbol
+      deployer,
       ethers.parseUnits("1000000", 18), // initialSupply
     ],
     {
       initializer: "initialize",
-      // kind: "uups"
+      kind: "uups"
     }
   );
     await contract.waitForDeployment();
